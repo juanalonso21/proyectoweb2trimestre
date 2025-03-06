2aed9562f16bc72bac52b19e55510e83c4e68e81
juanalonso21 <juanalonsopulpi@gmail.com>
2025-03-06 17:18:38 +0100

    Parte back creada

---
+++ backend/backendcomercio/src/main/java/com/juanjosu/backendcomercio/BackendcomercioApplication.java
@@ -0,0 +1,13 @@
+package com.juanjosu.backendcomercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendcomercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendcomercioApplication.class, args);
+	}
+
+}
---
+++ backend/backendcomercio/src/test/java/com/juanjosu/backendcomercio/BackendcomercioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanjosu.backendcomercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendcomercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
