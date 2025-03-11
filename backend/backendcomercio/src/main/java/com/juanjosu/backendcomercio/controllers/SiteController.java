6635c21848e67e5366b9f200148ce91a49050426
J-osu <josuedelacr96@gmail.com>
2025-03-11 05:55:39 +0100

    Controlador creado a la espera de ajustes. Index casi terminado, ajustado de mediaquery. Páginas conectadas al index ( mantenimiento,ect) creadas. ficheros css de sus respectivos html creados, header y footer ajustados y responsivados.

---
+++ backend/backendcomercio/src/main/java/com/juanjosu/backendcomercio/controllers/SiteController.java
@@ -0,0 +1,13 @@
+package com.juanjosu.backendcomercio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SiteController {
+    
+    @GetMapping("/")
+    public String home(){
+        return "/www/site/index";
+    }
+}
