68bc81d4110d2956ada2f746380c53ae718a1102
HimilceSanchez <himisan27@gmail.com>
2025-03-14 09:11:24 +0100

    HomePage.jsx cambios

--- frontend/comercioOnline/src/views/HomePage.jsx
+++ frontend/comercioOnline/src/views/HomePage.jsx
@@ -4,6 +4,7 @@ import CarouselComponent from "../components/CarouselComponent";
 import ProductList from "../components/ProductList";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Contact from "../components/Contact";
 import "../css/HomePage.css"; // Ruta corregida
 import pageData from "../data/PageData.json"; // Importa el JSON
 
@@ -24,6 +25,9 @@ const HomePage = () => {
         <h2 className="text-center mb-4">Principales categorías</h2>
         <ProductList products={pageData.featuredProducts} />
       </Container>
+
+      {/* Contacto */}
+      <Contact/>
 
       {/* Footer */}
       <Footer />
