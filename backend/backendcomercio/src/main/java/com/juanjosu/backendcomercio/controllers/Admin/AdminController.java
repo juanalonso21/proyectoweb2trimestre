9a8140057a43d3f568f113dbc158a1784af8cd95
J-osu <josuedelacr96@gmail.com>
2025-03-17 05:13:05 +0100

    Reconstruyendo.

---
+++ backend/backendcomercio/src/main/java/com/juanjosu/backendcomercio/controllers/Admin/AdminController.java
@@ -0,0 +1,12 @@
+package com.juanjosu.backendcomercio.controllers.Admin;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AdminController {
+    
+    @GetMapping("/admin")
+    public String admin() {
+        System.out.println("admin");
+        return "/admin/site/index";
+    }
+}
